dc63caa3f4f9b805b9357313c37f8621c6cd0d15
Єгор Оводов <yehor.ovodov.25@kse.org.ua>
2026-04-09 23:40:32 +0300

    Initial Commit

---
+++ SillyNotes/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SillyNotes
+//
+//  Created by Єгор Оводов on 9/4/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SillyNotes/SillyNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SillyNotesApp.swift
+//  SillyNotes
+//
+//  Created by Єгор Оводов on 9/4/26.
+//
+
+import SwiftUI
+
+@main
+struct SillyNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
