b7793ae18b8407d0209b959ff9d5c83896e141ca
Єгор Оводов <yehor.ovodov.25@kse.org.ua>
2026-04-10 01:14:40 +0300

    implemented viewmodel

---
+++ SillyNotes/ViewModels/ViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  ViewModel.swift
+//  SillyNotes
+//
+//  Created by Єгор Оводов on 10/4/26.
+//
+
+class NotesManager{
+    let notesList: [Note] = Note.exampleNotes
+    
+    func open(note: Note) -> String{
+        var win = Wheel.spinWheel(chanceOfWinning: 70)
+        
+        if win{
+            return note.content
+        } else {
+            return Scrambler.сruelScramble(input: note.content)
+        }
+    }
+}
